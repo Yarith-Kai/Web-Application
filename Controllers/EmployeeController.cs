faf7fac73ae7ad9c835aaf1b034c5587cbce0582
Yarith-Kai <58524326+Yarith-Kai@users.noreply.github.com>
2019-12-11 22:40:41 +0700

    WebApplication
    WebApplication

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+        EmployeeDB empDB = new EmployeeDB();
+        public JsonResult List()
+        {
+            return Json(empDB.ListAll(), JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Add_new()
+        {
+            return View();
+        }
+        public JsonResult Add(Employee emp)
+        {
+            return Json(empDB.Add(emp), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Models/EmployeeDB.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class EmployeeDB
+    {
+        string cs = ConfigurationManager.ConnectionStrings["winAuth"].ConnectionString;
+        public List<Employee> ListAll()
+        {
+            List<Employee> lst = new List<Employee>();
+
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+                SqlCommand com = new SqlCommand("SelectEmployee", con);
+                com.CommandType = CommandType.StoredProcedure;
+                SqlDataReader rdr = com.ExecuteReader();
+                while (rdr.Read())
+                {
+                    lst.Add(new Employee
+                    {
+                        EmployeeID = Convert.ToInt32(rdr["EmployeeId"]),
+                        Name = rdr["Name"].ToString(),
+                        Age = Convert.ToInt32(rdr["Age"]),
+                        State = rdr["State"].ToString(),
+                        Country = rdr["Country"].ToString(),
+                    });
+                }
+                return lst;
+            }
+        }
+        public int Add(Employee emp)
+        {
+            int i;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+                SqlCommand com = new SqlCommand("InsertUpdateEmployee", con);
+                com.CommandType = CommandType.StoredProcedure;
+                com.Parameters.AddWithValue("@Id", emp.EmployeeID);
+                com.Parameters.AddWithValue("@Name", emp.Name);
+                com.Parameters.AddWithValue("@Age", emp.Age);
+                com.Parameters.AddWithValue("@State", emp.State);
+                com.Parameters.AddWithValue("@Country", emp.Country);
+                com.Parameters.AddWithValue("@Action", "Insert");
+                i = com.ExecuteNonQuery();
+            }
+            return i;
+        }
+    }
+}
